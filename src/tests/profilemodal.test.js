0f810084ca378ad4bc5037c1795c278e6e4530da
YevhenLisitskyiDev <yevhen.lisitskyi.dev@gmail.com>
2021-10-08 17:11:33 +0300

    update some tests

--- src/tests/profilemodal.test.js
+++ src/tests/profilemodal.test.js
@@ -5,7 +5,31 @@ import ProfileModal from "../Components/Modal/ProfileModal";
 import { BrowserRouter as Router } from "react-router-dom";
 import ProfilesProvider from "../Contexts/ProfilesContext";
 
+const renderComponent = (onClose) =>
+  render(
+    <AuthProvider>
+      <ProfilesProvider>
+        <Router>
+          <ProfileModal onClose={onClose} />
+        </Router>
+      </ProfilesProvider>
+    </AuthProvider>
+  );
+
 describe("Profile Modal", () => {
+  test("Close Modal if successful", async () => {
+    axios.post = jest.fn(() =>
+      Promise.resolve({
+        data: {},
+      })
+    );
+    const onClose = jest.fn(() => {});
+    const screen = renderComponent(onClose);
+    const input = screen.getByText("gender:");
+    await waitFor(() => fireEvent.submit(input));
+    expect(onClose).toHaveBeenCalled();
+  });
+
   test("Show alert if error", async () => {
     axios.post = jest.fn(() =>
       Promise.reject({
@@ -14,17 +38,7 @@ describe("Profile Modal", () => {
         },
       })
     );
-
-    const screen = render(
-      <AuthProvider>
-        <ProfilesProvider>
-          <Router>
-            <ProfileModal />
-          </Router>
-        </ProfilesProvider>
-      </AuthProvider>
-    );
-
+    const screen = renderComponent();
     const input = screen.getByText("gender:");
     fireEvent.submit(input);
     await waitFor(() => screen.getByText("Error"));
--- src/tests/usermodal.test.js
+++ src/tests/usermodal.test.js
@@ -4,11 +4,39 @@ import UserProvider from "../Contexts/UserContext";
 import UserModal from "../Components/Modal/UserModal";
 import axios from "axios";
 
+const userToEdit = {
+  _id: "1",
+};
+const renderComponent = (onClose, setEditedUser) =>
+  render(
+    <AuthProvider>
+      <UserProvider>
+        <UserModal
+          userToEdit={userToEdit}
+          onClose={onClose}
+          setEditedUser={setEditedUser}
+        />
+      </UserProvider>
+    </AuthProvider>
+  );
+
 describe("User Modal", () => {
+  test("Close Modal if successful", async () => {
+    axios.put = jest.fn(() =>
+      Promise.resolve({
+        data: {},
+      })
+    );
+    const onClose = jest.fn(() => {});
+    const setEditedUser = jest.fn(() => {});
+    const screen = renderComponent(onClose, setEditedUser);
+    const input = screen.getByText("role:");
+    await waitFor(() => fireEvent.submit(input));
+    expect(onClose).toHaveBeenCalled();
+    expect(setEditedUser).toHaveBeenCalled();
+  });
+
   test("Show alert if error", async () => {
-    const userToEdit = {
-      _id: "1",
-    };
     axios.put = jest.fn(() =>
       Promise.reject({
         response: {
@@ -16,13 +44,7 @@ describe("User Modal", () => {
         },
       })
     );
-    const screen = render(
-      <AuthProvider>
-        <UserProvider>
-          <UserModal userToEdit={userToEdit} />
-        </UserProvider>
-      </AuthProvider>
-    );
+    const screen = renderComponent();
     const input = screen.getByText("role:");
     fireEvent.submit(input);
     await waitFor(() => screen.getByText("Error"));
